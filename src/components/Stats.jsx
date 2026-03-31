a9e21646d12aa28440061a592c5df21d694cf069
Mizanur Rahman <mdmizanurrahmn5215@gmail.com>
2026-03-31 12:48:49 +0600

    design stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,9 +3,24 @@ import React from 'react'
 const Stats = () => {
   return (
     <>
-    <div className='gradient'>
-         <div className='container mx-auto'>
-            dfa
+    <div className='gradient my-10'>
+         <div className='container mx-auto flex items-center justify-around py-10  text-white '>
+            
+                <div className=''>
+                    <h1 className='text-4xl font-bold mb-4'>50K+</h1>
+                    <p>Active Users</p>
+                </div>
+                <div className='border-r border-gray-400 py-7'></div>
+                <div>
+                    <h1 className='text-4xl font-bold mb-4'>200K+</h1>
+                    <p>Premium Tools</p>
+                </div>
+                 <div className='border-r border-gray-400 py-7'></div>
+                <div>
+                    <h1 className='text-4xl font-bold mb-4 '>4.9</h1>
+                    <p>Rating</p>
+                </div>
+          
          </div>
     </div>
     </>
